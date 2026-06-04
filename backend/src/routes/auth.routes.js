e076c44d525d40c48e98dd89f7c49f7912595633
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-04 17:57:38 +0530

    5

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,4 +3,7 @@ import { registerController } from '../controllers/auth.controller.js'
 let router =express.Router()
 
 router.post('/register',registerController)
+router.get('/',(req,res)=>{
+    res.send("Nihal bhai server connected hai")
+})
 export default router
