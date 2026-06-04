f791b6dac3960eacaa938cc9677f1b92fb027801
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-04 17:32:30 +0530

    20

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,14 @@ import express from 'express'
 import authRoutes from '../src/routes/auth.routes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 let app=express()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }))
 app.use('/api/auth',authRoutes)
