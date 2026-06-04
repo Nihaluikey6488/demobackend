2f5ddbabf32c40e7fcbb68b8ac95763dcf93fcb9
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-04 17:37:32 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import app from "./src/app.js";
 import connectDb from './src/config/db.js';
 connectDb()
-app.listen(3000,()=>{
+let port =process.env.PORT || 4000
+app.listen(port,()=>{
     console.log("Server is running on port 3000")
 })
